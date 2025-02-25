082dca3d5e9629a7af3760a72663f0eee57556ae
Taveirasson <lucaotaveira@gmail.com>
2025-02-25 20:59:51 -0300

    segundo commit Hello

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,14 +15,21 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$SoundJump.play()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	velocity.x = direction * SPEED
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play()
+	elif direction < 0:
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.stop()
+		
 
 	move_and_slide()
